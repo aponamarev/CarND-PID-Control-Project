21803acaa9c971a51bfc74eb73e07ed8896bbe53
aponamarev <alex.ponamaryov@gmail.com>
2017-11-09 14:46:17 -0500

    Fully functioning PID controller with twiddle straw man.

--- src/PID.h
+++ src/PID.h
@@ -1,46 +1,57 @@
 #ifndef PID_H
 #define PID_H
+#include <vector>
 
 class PID {
 public:
-  /*
-  * Errors
-  */
-  double p_error;
-  double i_error;
-  double d_error;
-
-  /*
-  * Coefficients
-  */ 
-  double Kp;
-  double Ki;
-  double Kd;
-
-  /*
-  * Constructor
-  */
-  PID();
-
-  /*
-  * Destructor.
-  */
-  virtual ~PID();
-
-  /*
-  * Initialize PID.
-  */
-  void Init(double Kp, double Ki, double Kd);
-
-  /*
-  * Update the PID error variables given cross track error.
-  */
-  void UpdateError(double cte);
-
-  /*
-  * Calculate the total PID error.
-  */
-  double TotalError();
+    /*
+     * Errors
+     */
+    double p_error;
+    double i_error;
+    double d_error;
+    
+    /*
+     * Coefficients
+     */
+    double Kp;
+    double Ki;
+    double Kd;
+    
+    /*
+     * Constructor
+     */
+    PID();
+    
+    /*
+     * Destructor.
+     */
+    virtual ~PID();
+    
+    /*
+     * Initialize PID.
+     */
+    void Init(double Kp, double Ki, double Kd);
+    
+    /*
+     * Update the PID error variables given cross track error.
+     */
+    void UpdateError(double cte);
+    
+    /*
+     * Calculate the total PID error.
+     */
+    double TotalError();
+    
+private:
+    bool is_initialize;
+    double twiddle_t;
+    double twiddle_error;
+    double best_error;
+    std::vector<double> dp;
+    int twiddle_id;
+    bool added_;
+    bool subtracted_;
 };
 
 #endif /* PID_H */
